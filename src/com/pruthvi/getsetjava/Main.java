6db7e71fba66a5dc5b9484c2236923d5a69b8fff
Pruthvi <pruthvishetty5656@gmail.com>
2021-07-13 03:30:31 +0530

    First Commit! Added a hello World Class, made a Mathops in a separate file and used in main function!

---
+++ src/com/pruthvi/getsetjava/Main.java
@@ -0,0 +1,13 @@
+package com.pruthvi.getsetjava;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+
+        int sum = mathOps.getSum(1,2);
+        System.out.println(sum);
+    }
+}
---
+++ src/com/pruthvi/getsetjava/mathOps.java
@@ -0,0 +1,19 @@
+package com.pruthvi.getsetjava;
+
+public class mathOps {
+    public static int getSum(int a,int b){
+        return a+b;
+    }
+    public static int getDiff(int a,int b){
+        return a-b;
+    }
+    public static int getMul(int a,int b){
+        return a*b;
+    }
+    public static int getDiv(int a,int b){
+        return a/b;
+    }
+    public static int getRem(int a,int b){
+        return a%b;
+    }
+}
